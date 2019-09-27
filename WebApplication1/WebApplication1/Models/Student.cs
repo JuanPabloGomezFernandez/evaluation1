750418a28aa609b6a072d5ebe1eb9c818af367b4
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 16:48:21 -0400

    vdbfd

---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public enum TypeGender
+    {
+       
+        Male,
+        Female,
+     
+
+
+    }
+
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [required]
+        public string Name { get; set; }
+
+        [required]
+        public TypeGender Gender { get; set; }
+
+        [required]
+        [DataType(DataType.Date)]
+
+        public int Birthday { get; set; }
+        [required]
+        public virtual ICollection< Grade> Grades{ get; set;}
+
+        }
+
+
+    }
---
+++ WebApplication1/WebApplication2/Models/StudentApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public enum TypeGender
+    {
+
+        Male,
+        Female,
+    }
+
+    public class StudentApi
+    {
+        public class Student
+        {
+            [Key]
+            public string Name { get; set; }
+            [required]
+            public int TypeGender { get; set; }
+            [required]
+            public int Birthday { get; set; }
+
+
+
+        }
+
+
+    }
+}
